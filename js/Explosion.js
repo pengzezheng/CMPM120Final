2103a12cca98d433daea6bec4f40d4a235fae6d2
Pengze Zheng <pzheng2@ucsc.edu>
2018-08-06 18:21:00 -0700

    Explosion.js works

--- js/Explosion.js
+++ js/Explosion.js
@@ -6,7 +6,6 @@ function Explosion(game, key) {
 	game.physics.enable(this, Phaser.Physics.ARCADE);
 	this.enableBody = true;
 	this.body.collideWorldBounds = false;
-	//this.explosion.play('', 0, 1, false);
 	//counter 
 	this.count = 0;
 }
@@ -17,11 +16,9 @@ Explosion.prototype.constructor = Explosion;
 
 Explosion.prototype.update = function() { 
 		//explosion animation/size change
-		if(this.count < 70) {
-				/*col = Phaser.Color.getRandomColor();
-				this.tint = col;*/
+		if(this.count < 40) {
 				if(this.count < 30) {
-						this.scale.setTo(this.count*0.1, this.count*0.1);
+					this.scale.setTo(0.1+this.count*0.1, 0.1+this.count*0.1);
 				}
 				this.count++;
 		}
