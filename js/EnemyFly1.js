85d04c09c50982539f4bc7b974d195443490d0d5
Pengze Zheng <pzheng2@ucsc.edu>
2018-08-06 17:34:00 -0700

    EnemyFly1.js works, fixed the bomb dropping

--- js/EnemyFly1.js
+++ js/EnemyFly1.js
@@ -4,15 +4,17 @@ function EnemyFly1(game, key){
 	game.physics.enable(this);//enable physics
 	this.enableBody=true;
 	this.body.velocity.x = -50;
+	count=0;
 }
 
 EnemyFly1.prototype = Object.create(Phaser.Sprite.prototype);// make prototype
 EnemyFly1.prototype.constructor = EnemyFly1;
 
 EnemyFly1.prototype.update = function() {
-	if(this.body.x>player.x+100 && this.body.x<player.x+102){
+	if(this.body.x>=player.x +100 &&this.body.x<=player.x +102 &&count==0){
 		bomb=new Bomb(game, "diamond");
 		bomb.reset(this.body.x,this.body.y);
 		game.add.existing(bomb);
+		count++;
 	}
 }
