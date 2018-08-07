75b7407d1ad5ac95ec6e2cb5490061b4d09336a3
Pengze Zheng <pzheng2@ucsc.edu>
2018-08-06 22:00:04 -0700

    asdf

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create
 var hp =5;
 function preload() {
     game.load.image('sky', 'assets/background.png');
-    game.load.image('ground', 'assets/platforms.png');
-    game.load.image('star', 'assets/monster.png');
+    game.load.image('ground', 'assets/platform.png');
+    game.load.image('star', 'assets/mob.png');
     game.load.image('diamond', 'assets/diamond.png');
     game.load.image('firstaid', 'assets/firstaid.png');
     game.load.image('candle', 'assets/stand.png');
