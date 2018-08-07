70cb963274ce56fb15591394f3824141ea94afb4
Pengze Zheng <pzheng2@ucsc.edu>
2018-08-06 18:19:30 -0700

    Explosion.js works

--- js/Bomb.js
+++ js/Bomb.js
@@ -3,6 +3,7 @@ function Bomb(game, key){
 	game.physics.enable(this, Phaser.Physics.ARCADE);//enable physics
 	this.enableBody=true;
 	this.body.gravity.y=100;
+	count=0;
 }
 
 function explo(bomb, player){//kill bullet and do damage
@@ -17,5 +18,8 @@ Bomb.prototype = Object.create(Phaser.Sprite.prototype);// make prototype
 Bomb.prototype.constructor = Bomb;
 
 Bomb.prototype.update = function() {
-	 game.time.events.add(Phaser.Timer.SECOND * 2, explo, this);
+	if(count <2){
+	 	game.time.events.add(Phaser.Timer.SECOND * 2, explo, this);
+	 	count++;
+	}
 }	
