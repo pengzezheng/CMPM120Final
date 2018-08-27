2e85d77fceb832080dacb7014e36fd5652afa4aa
Pengze Zheng <pzheng2@ucsc.edu>
2018-08-27 14:59:56 -0700

    enemy app

---
+++ js/EnemyDark.js
@@ -0,0 +1,17 @@
+function EnemyDark(game, key){
+	Phaser.Sprite.call(this, game, 400, 500, key);// call phaser sprite
+	this.scale.setTo(0.5);
+	game.physics.enable(this, Phaser.Physics.ARCADE);//enable physics
+	this.enableBody=true;
+	//timer=game.time.create();
+}
+
+	
+EnemyDark.prototype = Object.create(Phaser.Sprite.prototype);// make prototype
+EnemyDark.prototype.constructor = EnemyDark;
+
+EnemyDark.prototype.update = function() {
+	if (){
+		game.physics.arcade.moveToObject(this,player);
+	}
+} 
