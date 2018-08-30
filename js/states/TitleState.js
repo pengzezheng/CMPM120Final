705179e19cc0a4dc4bfae0f68445259d79956c24
jan-yu <jan.yu415@gmail.com>
2018-08-30 16:16:58 -0700

    code cleanup.

--- js/states/TitleState.js
+++ js/states/TitleState.js
@@ -10,15 +10,6 @@ TitleState.prototype = {
 	 * In this case, it simply displays an image.
 	 */
 	create: function() {
-		/*var titleText = game.add.text(game.width/2, game.height/2, 'LAST LIGHT', {font: 'Helvetica', fontSize: '48px', fill: '#fff'});
-		titleText.anchor.set(0.5);
-
-		var instructText = game.add.text(game.width/2, game.height/2 + 48, 'Use the Arrow Keys to move around.', {font: 'Helvetica', fontSize: '24px', fill: '#fff'});
-		instructText.anchor.set(0.5);
-
-		var playText = game.add.text(game.width/2, game.height*.8, 'Press the SPACE BAR to Start', {font: 'Helvetica', fontSize: '24px', fill: '#fff'});
-		playText.anchor.set(0.5);*/
-
 		// adds and sets the title screen image.
 		var sky = game.add.sprite(0, 0, 'title screen');
 		sky.scale.setTo(1.2,1.08);
