3e037dcffd78052971536295d249632874165388
jan-yu <jan.yu415@gmail.com>
2018-08-30 01:50:04 -0700

    code cleanup.

--- js/states/LevelHeight.js
+++ js/states/LevelHeight.js
@@ -48,35 +48,40 @@ LevelHeight.prototype = {
 	   	//a.debug=true;
 	   	map.setCollisionByExclusion([], true, 'layer3', true);
 
-	    aEGrab =new EnemyGrab(game,'star');
+	    // constructs flying enemies in specific coordinates of the map.
+	    aEGrab = new EnemyGrab(game,'star');
+	    // specifies the x coordinate.
 	    aEGrab.x = (14 * 32);
+	    // specifies the y coordinate.
 	    aEGrab.y = (115 * 32);
+	    // adds the existing flying enemy object.
 	    game.add.existing(aEGrab);
-	    aEGrab =new EnemyGrab(game,'star');
+	    // constructs more of the above objects.
+	    aEGrab = new EnemyGrab(game,'star');
 	    aEGrab.x = (22 * 32);
 	    aEGrab.y = (115 * 32);
 	    game.add.existing(aEGrab);
-	    aEGrab =new EnemyGrab(game,'star');
+	    aEGrab = new EnemyGrab(game,'star');
 	    aEGrab.x = (10 * 32);
 	    aEGrab.y = (111 * 32);
 	    game.add.existing(aEGrab);
-	    aEGrab =new EnemyGrab(game,'star');
+	    aEGrab = new EnemyGrab(game,'star');
 	    aEGrab.x = (27 * 32);
 	    aEGrab.y = (110 * 32);
 	    game.add.existing(aEGrab);
-	    aEGrab =new EnemyGrab(game,'star');
+	    aEGrab = new EnemyGrab(game,'star');
 	    aEGrab.x = (14 * 32);
 	    aEGrab.y = (103 * 32);
 	    game.add.existing(aEGrab);
-	    aEGrab =new EnemyGrab(game,'star');
+	    aEGrab = new EnemyGrab(game,'star');
 	    aEGrab.x = (4 * 32);
 	    aEGrab.y = (99 * 32);
 	    game.add.existing(aEGrab);
-	    aEGrab =new EnemyGrab(game,'star');
+	    aEGrab = new EnemyGrab(game,'star');
 	    aEGrab.x = (26 * 32);
 	    aEGrab.y = (98 * 32);
 	    game.add.existing(aEGrab);
-	    aEGrab =new EnemyGrab(game,'star');
+	    aEGrab = new EnemyGrab(game,'star');
 	    aEGrab.x = (21 * 32);
 	    aEGrab.y = (91 * 32);
 	    game.add.existing(aEGrab);
@@ -84,112 +89,118 @@ LevelHeight.prototype = {
 	    aEGrab.x = (20 * 32);
 	    aEGrab.y = (81 * 32);
 	    game.add.existing(aEGrab);
-	    aEGrab =new EnemyGrab(game,'star');
+	    aEGrab = new EnemyGrab(game,'star');
 	    aEGrab.x = (8 * 32);
 	    aEGrab.y = (72 * 32);
 	    game.add.existing(aEGrab);
-	    aEGrab =new EnemyGrab(game,'star');
+	    aEGrab = new EnemyGrab(game,'star');
 	    aEGrab.x = (27 * 32);
 	    aEGrab.y = (66 * 32);
 	    game.add.existing(aEGrab);
-	    aEGrab =new EnemyGrab(game,'star');
+	    aEGrab = new EnemyGrab(game,'star');
 	    aEGrab.x = (7 * 32);
 	    aEGrab.y = (63 * 32);
 	    game.add.existing(aEGrab);
-	    aEGrab =new EnemyGrab(game,'star');
+	    aEGrab = new EnemyGrab(game,'star');
 	    aEGrab.x = (22 * 32);
 	    aEGrab.y = (57 * 32);
 	    game.add.existing(aEGrab);
-	    aEGrab =new EnemyGrab(game,'star');
+	    aEGrab = new EnemyGrab(game,'star');
 	    aEGrab.x = (8 * 32);
 	    aEGrab.y = (55 * 32);
 	    game.add.existing(aEGrab);
-	    aEGrab =new EnemyGrab(game,'star');
+	    aEGrab = new EnemyGrab(game,'star');
 	    aEGrab.x = (22 * 32);
 	    aEGrab.y = (51 * 32);
 	    game.add.existing(aEGrab);
-	    aEGrab =new EnemyGrab(game,'star');
+	    aEGrab = new EnemyGrab(game,'star');
 	    aEGrab.x = (16 * 32);
 	    aEGrab.y = (50 * 32);
 	    game.add.existing(aEGrab);
-	    aEGrab =new EnemyGrab(game,'star');
+	    aEGrab = new EnemyGrab(game,'star');
 	    aEGrab.x = (6 * 32);
 	    aEGrab.y = (48 * 32);
 	    game.add.existing(aEGrab);
-	    aEGrab =new EnemyGrab(game,'star');
+	    aEGrab = new EnemyGrab(game,'star');
 	    aEGrab.x = (22 * 32);
 	    aEGrab.y = (47 * 32);
 	    game.add.existing(aEGrab);
-	    aEGrab =new EnemyGrab(game,'star');
+	    aEGrab = new EnemyGrab(game,'star');
 	    aEGrab.x = (19 * 32);
 	    aEGrab.y = (42 * 32);
 	    game.add.existing(aEGrab);
-	    aEGrab =new EnemyGrab(game,'star');
+	    aEGrab = new EnemyGrab(game,'star');
 	    aEGrab.x = (14 * 32);
 	    aEGrab.y = (115 * 32);
 	    game.add.existing(aEGrab);
-	    aEGrab =new EnemyGrab(game,'star');
+	    aEGrab = new EnemyGrab(game,'star');
 	    aEGrab.x = (2 * 32);
 	    aEGrab.y = (41 * 32);
 	    game.add.existing(aEGrab);
-	    aEGrab =new EnemyGrab(game,'star');
+	    aEGrab = new EnemyGrab(game,'star');
 	    aEGrab.x = (23 * 32);
 	    aEGrab.y = (40 * 32);
 	    game.add.existing(aEGrab);
-	    aEGrab =new EnemyGrab(game,'star');
+	    aEGrab = new EnemyGrab(game,'star');
 	    aEGrab.x = (22 * 32);
 	    aEGrab.y = (39 * 32);
 	    game.add.existing(aEGrab);
-	    aEGrab =new EnemyGrab(game,'star');
+	    aEGrab = new EnemyGrab(game,'star');
 	    aEGrab.x = (14 * 32);
 	    aEGrab.y = (37 * 32);
 	    game.add.existing(aEGrab);
-	    aEGrab =new EnemyGrab(game,'star');
+	    aEGrab = new EnemyGrab(game,'star');
 	    aEGrab.x = (5 * 32);
 	    aEGrab.y = (31 * 32);
 	    game.add.existing(aEGrab);
-	    aEGrab =new EnemyGrab(game,'star');
+	    aEGrab = new EnemyGrab(game,'star');
 	    aEGrab.x = (17 * 32);
 	    aEGrab.y = (25 * 32);
 	    game.add.existing(aEGrab);
-	    aEGrab =new EnemyGrab(game,'star');
+	    aEGrab = new EnemyGrab(game,'star');
 	    aEGrab.x = (12 * 32);
 	    aEGrab.y = (20 * 32);
 	    game.add.existing(aEGrab);
-	    aEGrab =new EnemyGrab(game,'star');
+	    aEGrab = new EnemyGrab(game,'star');
 	    aEGrab.x = (19 * 32);
 	    aEGrab.y = (16 * 32);
 	    game.add.existing(aEGrab);
-	    aEGrab =new EnemyGrab(game,'star');
+	    aEGrab = new EnemyGrab(game,'star');
 	    aEGrab.x = (10 * 32);
 	    aEGrab.y = (13 * 32);
 	    game.add.existing(aEGrab);
-	    aEGrab =new EnemyGrab(game,'star');
+	    aEGrab = new EnemyGrab(game,'star');
 	    aEGrab.x = (17 * 32);
 	    aEGrab.y = (10 * 32);
 	    game.add.existing(aEGrab);
-	    aEGrab =new EnemyGrab(game,'star');
+	    aEGrab = new EnemyGrab(game,'star');
 	    aEGrab.x = (21 * 32);
 	    aEGrab.y = (9 * 32);
 	    game.add.existing(aEGrab);
-	    aEGrab =new EnemyGrab(game,'star');
+	    aEGrab = new EnemyGrab(game,'star');
 	    aEGrab.x = (5 * 32);
 	    aEGrab.y = (5 * 32);
 	    game.add.existing(aEGrab);
 
+	   	// adds the player in the game.
 	   	player = new Player(game,'player',450,3900);
-	   	player.body.gravity.y=1750;
+	   	// sets the player's initial gravity.
+	   	player.body.gravity.y = 1750;
+	   	// add the existing player.
     	game.add.existing(player);
+    	// makes the camera follow the player.
     	game.camera.follow(player, Phaser.Camera.FOLLOW_LOCKON);
 
+    	// constructs checkpoints at specific areas of the map.
     	checkpoint = game.add.sprite(461,3815,'checkpoint');
+    	// enables arcade physics for the checkpoint.
 	    game.physics.arcade.enable(checkpoint);
+	    //enables the checkpoint body.
 		checkpoint.enableBody = true;
-
+		//constructs more of the above objects.
 		checkpoint2 = game.add.sprite(768,2407,'checkpoint');
 	    game.physics.arcade.enable(checkpoint2);
 		checkpoint2.enableBody = true;
-
 		checkpoint3 = game.add.sprite(503,711,'checkpoint');
 	    game.physics.arcade.enable(checkpoint3);
 		checkpoint3.enableBody = true;
@@ -282,20 +293,19 @@ LevelHeight.prototype = {
 		//game.physics.arcade.overlap(player,checkpoint4,this.reachCheckpoint4,null,this);
 
 		// allows the player to fall off the map.
-
 		game.physics.arcade.collide(player,layer3);
 		this.updateShadowTexture();
-		if(player.y >= 3950){
+		if(player.y >= 3950) {
 	    	player.body.collideWorldBounds = false;
 	    }
 
 	    // kills the player when they fall.
-	    if(player.y >= 4100){
+	    if(player.y >= 4100) {
 	    	widthLife.width = -1;
 	    	player.body.velocity.y=0;
 	    }
 
-	    if(widthLife.width<=0 ){
+	    if(widthLife.width<=0 ) {
 			widthLife.width = 0;
 			dead = true;
 			player.animations.stop(null,true);
@@ -304,34 +314,38 @@ LevelHeight.prototype = {
 			player.alpha = 0;
 	
 			//create a custom timer
-
-
 			timer = game.time.create();
-				//create a event 3s from now
+			//create a event 3s from now
 			timeEvent = timer.add(Phaser.Timer.SECOND*3,this.endTimer,this);
 			timer.start();
 
+		    if(counterh == 4) {
+				l5.destroy();
+			}
+			if(counterh == 3) {
+				l4.destroy();
+			}
+			if(counterh == 2) {
+				l3.destroy();
+			}
+			if(counterh == 1) {
+				l2.destroy();
+			}
+			if(counterh == 0) {
+				l1.destroy();
+				BGM1.stop();
+				game.state.start("GameOver");
+			}
+		}
+	},
 
-	    if(counterh==4){
-			l5.destroy();
-		}
-		if(counterh==3){
-			l4.destroy();
-		}
-		if(counterh==2){
-			l3.destroy();
-		}
-		if(counterh==1){
-			l2.destroy();
-		}
-		if(counterh==0){
-			l1.destroy();
-			BGM1.stop();
-			game.state.start("GameOver");
-		}
-	}
-},
-	reachCheckpoint: function(player,checkpoint){
+	/**
+	 * The reachCheckpoint function creates a new checkpoint for the player to start in when player
+	 * and checkpoint collides.
+	 * @param player: the player object
+	 * @param checkpoint: the checkpoint object
+	 */
+	reachCheckpoint: function(player,checkpoint) {
 		console.log("a");
 		//TempX = checkpoint.x;
     	//TempY = checkpoint.y;
@@ -341,13 +355,19 @@ LevelHeight.prototype = {
 		var saved=new Checkpoint(game,checkpoint.x,checkpoint.y-5,'checkpoint1');
 		game.add.existing(saved);
 
-	//saved.enableBody = true;
+		//saved.enableBody = true;
 		checkpoint.kill();
 		this.lights.add(saved);
 		saved.LIGHT_RADIUS = 50;
 
 	},
-	reachCheckpoint2: function(player,checkpoint2){
+	/**
+	 * The reachCheckpoint2 function creates a second checkpoint for the player to start in when the 
+	 * player and the checkpoint collides.
+	 * @param player: the player object
+	 * @param checkpoint: the checkpoint object
+	 */
+	reachCheckpoint2: function(player,checkpoint2) {
 		console.log("a");
 		//TempX = checkpoint2.x;
     	//TempY = checkpoint2.y;
@@ -357,14 +377,20 @@ LevelHeight.prototype = {
 		var saved2=new Checkpoint(game,checkpoint2.x,checkpoint2.y-5,'checkpoint1');
 		game.add.existing(saved2);
 
-	//saved.enableBody = true;
+		//saved.enableBody = true;
 		checkpoint2.kill();
 		this.lights.add(saved2);
 		saved2.LIGHT_RADIUS = 50;
 		
-
 	},
-	reachCheckpoint3: function(player,checkpoint3){
+
+	/**
+	 * The reachCheckpoint3 function starts the player at the third checkpoint when the player and the 
+	 * checkpoint collides.
+	 * @param player: the player object
+	 * @param checkpoint: the checkpoint object
+	 */
+	reachCheckpoint3: function(player,checkpoint3) {
 		console.log("a");
 		Ignite.play();
 		//TempX = checkpoint2.x;
@@ -374,14 +400,19 @@ LevelHeight.prototype = {
 		var saved3=new Checkpoint(game,checkpoint3.x,checkpoint3.y-5,'checkpoint1');
 		game.add.existing(saved3);
 
-	//saved.enableBody = true;
+		//saved.enableBody = true;
 		checkpoint3.kill();
 		this.lights.add(saved3);
 		saved3.LIGHT_RADIUS = 50;
-		
-
 	},
-	reachCheckpoint4: function(player,checkpoint4){
+
+	/**
+	 * The reachCheckpoint4 function starts the player at the fourth checkpoint when the player and 
+	 * the checkpoint collides.
+	 * @param player: the player object
+	 * @param checkpoint: the checkpoint object
+	 */
+	reachCheckpoint4: function(player,checkpoint4) {
 		console.log("a");
 		Ignite.play();
 		//TempX = checkpoint2.x;
@@ -391,7 +422,7 @@ LevelHeight.prototype = {
 		var saved4=new Checkpoint(game,checkpoint4.x,checkpoint4.y-5,'checkpoint1');
 		game.add.existing(saved4);
 
-	//saved.enableBody = true;
+		//saved.enableBody = true;
 		checkpoint4.kill();
 		this.lights.add(saved4);
 		saved4.LIGHT_RADIUS = 50;
@@ -399,18 +430,19 @@ LevelHeight.prototype = {
 
 	},
 
-	updateShadowTexture:function(){
+	/**
+	 * This function updates the shadow texture.
+	 */
+	updateShadowTexture:function() {
 		this.shadowTexture.context.fillStyle = 'rgb(0, 0, 0)';
 		this.shadowTexture.context.fillRect(0,0,game.world.width,game.world.height);
 		// this.shadowTexture.context.fillRect(2000,0,2000,1200);
 		// this.shadowTexture.context.fillRect(4000,0,2000,1200);
 		// this.shadowTexture.context.fillRect(6000,0,2000,1200);
 
-		
-
     	// Iterate through each of the lights and draw the glow
     	this.lights.forEach(function(m) {
-    		if(m == player){
+    		if(m == player) {
     			//Randomly change the radius each frame
         		playerRadius = this.LIGHT_RADIUS + this.game.rnd.integerInRange(1,10);
 
@@ -425,63 +457,62 @@ LevelHeight.prototype = {
         		this.shadowTexture.context.fill();
         		
        	 	}
-    
-        else{
-        	// Randomly change the radius each frame
-        	checkpointRadius = 100 + this.game.rnd.integerInRange(1,10);
+	        else {
+	        	// Randomly change the radius each frame
+	        	checkpointRadius = 100 + this.game.rnd.integerInRange(1,10);
 
-        	// Draw circle of light with a soft edge
-        	var gradient =this.shadowTexture.context.createRadialGradient(m.x, m.y,100 * 0.75,m.x, m.y, checkpointRadius);
-        	gradient.addColorStop(0, 'rgba(255, 255, 255, 1.0)');
-        	gradient.addColorStop(1, 'rgba(255, 255, 255, 0.0)');
+	        	// Draw circle of light with a soft edge
+	        	var gradient =this.shadowTexture.context.createRadialGradient(m.x, m.y,100 * 0.75,m.x, m.y, checkpointRadius);
+	        	gradient.addColorStop(0, 'rgba(255, 255, 255, 1.0)');
+	        	gradient.addColorStop(1, 'rgba(255, 255, 255, 0.0)');
 
-        	this.shadowTexture.context.beginPath();
-        	this.shadowTexture.context.fillStyle = gradient;
-        	this.shadowTexture.context.arc(m.x, m.y, checkpointRadius, 0, Math.PI*2);
-        	this.shadowTexture.context.fill();
+	        	this.shadowTexture.context.beginPath();
+	        	this.shadowTexture.context.fillStyle = gradient;
+	        	this.shadowTexture.context.arc(m.x, m.y, checkpointRadius, 0, Math.PI*2);
+	        	this.shadowTexture.context.fill();
+        	}
+    	}, this);
 
-        }
-    }, this);
-
-    // This just tells the engine it should update the texture cache
-    this.shadowTexture.dirty = true;
-    
-
+    	// This just tells the engine it should update the texture cache
+    	this.shadowTexture.dirty = true;
 	},
-	endTimer:function(){
-		timer.stop();
-		if(dead == true){
-		//player.body.collideWorldBounds = true;
-		timer.stop();
-		widthLife.width = totalLife;
-		counterh--;
-		player.alpha = 1;
-		player.facing = 'right';
-		this.lights.add(player);
 
-		if(HeightCheck == 0){
-			player.body.gravity.y=2250;
-    		player.x = 450;
-    		player.y = 3800;
-    	}else if(HeightCheck == 1){
-    		player.x = checkpoint.x;
-    		player.y = checkpoint.y - 50;
-    		player.body.gravity.y=2250;
-    	}else if(HeightCheck == 2){
-    		player.x = checkpoint2.x;
-    		player.y = checkpoint2.y - 50;
-    		player.body.gravity.y=2250;
-    	}else if(HeightCheck == 3){
-    		player.x = checkpoint3.x;
-    		player.y = checkpoint3.y - 50;
-    		player.body.gravity.y=2250;
-    	}else if(HeightCheck == 4){
-    		player.x = checkpoint4.x;
-    		player.y = checkpoint4.y - 50;
-    		player.body.gravity.y=2250;
-    	}
+	/**
+ 	 * Sets where the player respawns depending on the checkpoint reached.
+ 	 */
+	endTimer:function() {
+		timer.stop();
+		if(dead == true) {
+			//player.body.collideWorldBounds = true;
+			timer.stop();
+			widthLife.width = totalLife;
+			counterh--;
+			player.alpha = 1;
+			player.facing = 'right';
+			this.lights.add(player);
 
-	}
+			if(HeightCheck == 0) {
+				player.body.gravity.y=2250;
+	    		player.x = 450;
+	    		player.y = 3800;
+	    	} else if(HeightCheck == 1) {
+	    		player.x = checkpoint.x;
+	    		player.y = checkpoint.y - 50;
+	    		player.body.gravity.y=2250;
+	    	} else if(HeightCheck == 2) {
+	    		player.x = checkpoint2.x;
+	    		player.y = checkpoint2.y - 50;
+	    		player.body.gravity.y=2250;
+	    	} else if(HeightCheck == 3) {
+	    		player.x = checkpoint3.x;
+	    		player.y = checkpoint3.y - 50;
+	    		player.body.gravity.y=2250;
+	    	} else if(HeightCheck == 4) {
+	    		player.x = checkpoint4.x;
+	    		player.y = checkpoint4.y - 50;
+	    		player.body.gravity.y=2250;
+	    	}
+		}
 		dead = false;
 	},
 
